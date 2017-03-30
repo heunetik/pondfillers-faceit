08d5d73054da284abf5106689ef7a9449d9d2254
heunetik <heunetik@gmail.com>
2017-03-30 12:09:23 +0300

    Added kd_stats
    Added kd_stats function to display the player's K/D average of the player in the last 3 matches.

--- pondfillers/scripts.js
+++ pondfillers/scripts.js
@@ -1,8 +1,10 @@
 document.addEventListener('DOMContentLoaded', function () {
-    document.getElementById('pondfillers').addEventListener('click', home);
+    document.getElementById('pondfillers').addEventListener('click', kd_stats);
     document.getElementById('pondTS').addEventListener('click', teamspeak);
     document.getElementById('query').addEventListener('click', getUserName);
 });
+
+var un_id;
 
 function home()
 {
@@ -25,6 +27,8 @@ function faceit(uid_check){
 	xmlhttp.onreadystatechange = function () {
 	    if (this.readyState == 4 && this.status == 200) {
 	        myObj = JSON.parse(this.responseText);
+	        un_id = myObj.payload.guid;
+
 	        // document.getElementById("faceit_name").innerHTML ="";
 	        // document.getElementById("faceit_name").innerHTML = uid_check;
 	        document.getElementById("faceit").innerHTML ="";
@@ -39,4 +43,31 @@ function faceit(uid_check){
 	var url = "https://api.faceit.com/core/v1/nicknames/";
 	xmlhttp.open("GET", url + uid_check, true);
 	xmlhttp.send();
+}
+
+function kd_stats(){
+	var xmlhttp = new XMLHttpRequest();
+	xmlhttp.onreadystatechange = function () {
+	    if (this.readyState == 4 && this.status == 200) {
+	        jObj = JSON.parse(this.responseText);
+	        var kills = 0;
+	        var deaths = 0;
+	        var k_d = 0;
+	        for(var i=0; i<3; i++)
+	        {
+	        	kills = kills + parseFloat(jObj[i].i6);
+	        	deaths = deaths + parseFloat(jObj[i].i8);
+	        }
+	        k_d = kills / deaths;
+	        k_d = k_d.toFixed(2);
+	        var textz = document.getElementById("faceit").innerHTML;
+	        document.getElementById("faceit").innerHTML = textz + "<br> K/D " + k_d;
+	    }
+	};
+	var url = "https://api.faceit.com/stats/v1/stats/time/users/";
+	var url_p2 = "/games/csgo?page=0&size=3";
+	xmlhttp.open("GET", url + un_id + url_p2, true);
+	xmlhttp.send();
+	// 06b6394e-795d-4e8e-bfd3-79f9581607b1 = heuID
+	// 48f9db99-b075-4f86-9286-250692c14325 = RickyID
 }
